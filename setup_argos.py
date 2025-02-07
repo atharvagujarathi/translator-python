06535fdcd7f0ccd0fa630cef7d6ff0d63f08490d
atharvagujarathi <75681555+atharvagujarathi@users.noreply.github.com>
2025-02-07 23:23:41 +0530

    ok

--- setup_argos.py
+++ setup_argos.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from deep_translator import GoogleTranslator
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/translate")
 async def translate_text(data: dict):
